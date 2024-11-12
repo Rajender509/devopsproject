b0463dad145e555ddef1f50684575c5aabd8aff9
Rajender <rajendermadishetti999@gmail.com>
2024-11-12 16:12:20 +0530

    Multiply.java

--- Multiply.java
+++ Multiply.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class MultiplyNumbers {
+public class Multiply {
     public static void main(String[] args) {
         // Create a Scanner object to read input
         Scanner scanner = new Scanner(System.in);
